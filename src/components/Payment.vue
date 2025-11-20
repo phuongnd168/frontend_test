d7910b81d720743f30155aca43b5be0cfcb3776b
phuongnd168 <dauphaiphuong168@gmail.com>
2025-11-20 15:49:50 +0700

    update1

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,29 +1,14 @@
 <template>
-  <div>
-    <input v-model.number="amount" placeholder="Nhập số tiền" />
-    <button @click="pay">Thanh toán VNPAY</button>
-  </div>
+  <button @click="pay">Thanh toán VNPAY</button>
 </template>
 
 <script setup>
-import { ref } from "vue";
+const pay = async () => {
+  const res = await axios.post("https://your-api.com/api/vnpay/create", {
+    amount: 50000,
+    orderInfo: "Thanh toan don hang 123",
+  });
 
-const amount = ref(0);
-
-async function pay() {
-  const res = await fetch(
-    "https://karson-semicathartic-nondeprecatively.ngrok-free.dev/api/vnpay/create",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(amount.value),
-    }
-  );
-  const data = await res.json();
-  if (data.paymentUrl) {
-    window.location.href = data.paymentUrl;
-  } else {
-    alert("Tạo payment URL lỗi");
-  }
-}
+  window.location.href = res.data.paymentUrl;
+};
 </script>
